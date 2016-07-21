8bb70752e87c3747c8a7765e542d9004939eec9d
JazminBlue <you@example.com>
2016-07-21 02:30:25 +0200

    Agregando contenido

--- juego/models.py
+++ juego/models.py
@@ -6,7 +6,11 @@ class Partida(models.Model):
 
 	turno_de=models.CharField(max_length=20)
 	ID= models.CharField(max_lenght=10)
-	Estado(Registrando, Jugando,Terminado)=models.TextField()
+	Estado(Registrando,Jugando,Terminado)=models.TextField()
+	
+
+	def __str__(self):
+        return "{} ({})".format(self.turno_de, self.ID, self.Estado)
 
 class Jugador(models.Model):
 	
@@ -15,7 +19,8 @@ class Jugador(models.Model):
 	incognita=models.IntegerField()
 	activo=models.BoolField(CheckboxInput)
 
-
+	def __str__(self):
+        return "{} ({})".format(self.nombre, self.incognita)
 
 class Jugada(models.Model):
 	
@@ -23,7 +28,10 @@ class Jugada(models.Model):
 	apuesta=models.CharField()
 	bien=models.IntegerField()
 	regular=models.IntegerField()
+	
 
+	def __str__(self):
+        return "{} ({})".format(self.ronda, self.apuesta self.bien self.regular)
 
 
 	
