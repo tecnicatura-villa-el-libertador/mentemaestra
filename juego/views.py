19412b8cf6ef6ce6a49fb726810f348d9b4bf40a
lucas <luk-s.17@hotmail.com.ar>
2016-08-11 11:57:13 -0300

    cambios en el ingreso de numeros

--- juego/views.py
+++ juego/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def comenzar(request):
+	mensaje = "ingresar numero"
+	return render(request, 'comenzar.html',
+		{'mensaje':mensaje,
+		})
--- mentemaestra/urls.py
+++ mentemaestra/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
+from juego.views import comenzar
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^comenzar.html/', comenzar),
 ]
